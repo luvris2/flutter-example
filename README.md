c437be70ff8ebd5fa17b4830aeeea5dee6b880b8
EunbyeolKo <luvris2@naver.com>
2023-08-16 11:56:19 +0900

    add camera_preview_test
    add

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # flutter-example
+
+h3. flutter_camera_preview_test
+카메라를 통해 사진을 촬영하고
+촬영한 사진을 저장소에 저장하기
---
+++ flutter_camera_preview_test/android/app/src/main/kotlin/com/example/flutter_camera_preview_test/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_camera_preview_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
