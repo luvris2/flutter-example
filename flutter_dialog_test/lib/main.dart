5cbf3cb7d0fb875331a0496e4636a3c253152ca7
EunbyeolKo <luvris2@naver.com>
2023-11-22 17:51:35 +0900

    add
    add

---
+++ flutter_dialog_test/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialog_test/stateful_dialog.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: MainPage(),
+        ),
+      ),
+    ),
+  );
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  // 카운팅 값을 저장하는 변수
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+      child: Center(
+          child: Column(
+        children: [
+          // 카운팅된 값을 표시하는 영역
+          Text('값 : ${value.toString()}'),
+          ElevatedButton(
+            // 버튼 터치 시 다이얼로그 함수 호출
+            onPressed: () {
+              // 정의한 다이얼로그 함수
+              _dialogBuilder(context);
+            },
+            child: const Text('다이얼로그 출력'),
+          ),
+        ],
+      )),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    // 현재 화면 위에 보여줄 다이얼로그 생성
+    return showDialog<void>(
+      context: context,
+      builder: (context) {
+        // 빌더로 AlertDialog 위젯을 생성
+        return AlertDialog(
+          title: const Text('다이얼로그 제목'),
+          content: const Text('다이얼로그 내용'),
+          actions: [
+            ElevatedButton(
+              // 다이얼로그 내의 확인 버튼 터치 시 값 +1
+              onPressed: () {
+                setState(() => value++);
+              },
+              child: const Text('확인'),
+            ),
+            // 다이얼로그 내의 취소 버튼 터치 시 다이얼로그 화면 제거
+            OutlinedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('취소'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
---
+++ flutter_dialog_test/lib/stateful_dialog.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class MainPage2 extends StatefulWidget {
+  const MainPage2({super.key});
+
+  @override
+  State<MainPage2> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage2> {
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 100, 0, 0),
+      child: Center(
+          child: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              _dialogBuilder();
+            },
+            child: const Text('StatefulBuilder 다이얼로그'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              _dialogStfWidget();
+            },
+            child: const Text('StatefullWidget 다이얼로그'),
+          ),
+        ],
+      )),
+    );
+  }
+
+  Future<void> _dialogBuilder() {
+    return showDialog<void>(
+      //barrierColor: Colors.red,
+      context: context,
+      builder: (context) {
+        return StatefulBuilder(builder: (context, setState) {
+          return AlertDialog(
+            title: const Text('StatefulBuilder 활용'),
+            content: Text('다이얼로그 내에서 값 변경하기 : $value'),
+            actions: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() => value++);
+                },
+                child: const Text('확인'),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('취소'),
+              ),
+            ],
+          );
+        });
+      },
+    );
+  }
+
+  Future<dynamic> _dialogStfWidget() async {
+    await showDialog<void>(
+      context: context,
+      builder: (context) {
+        return const DialogPage();
+      },
+    );
+  }
+}
+
+class DialogPage extends StatefulWidget {
+  const DialogPage({super.key});
+
+  @override
+  State<DialogPage> createState() => _DialogPageState();
+}
+
+class _DialogPageState extends State<DialogPage> {
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('StatefulWidget 활용'),
+      content: Text('다이얼로그 내에서 값 변경하기 : $value'),
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            setState(() => value++);
+          },
+          child: const Text('확인'),
+        ),
+        OutlinedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('취소'),
+        ),
+      ],
+    );
+  }
+}
